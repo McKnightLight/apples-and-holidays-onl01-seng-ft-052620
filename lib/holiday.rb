cde54779426f7da93a2f60ab446b0e3e8bfb137d
Jonathan McKnight <themcknightrider@gmail.com>
2020-06-06 17:16:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,12 +60,18 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash.each do |season, holidays|
-    puts "#{season.to_s.capitalize!}:"
-    holidays.each do |holiday, items|
-      split_array = []
-      array = item.to_s.split("_")
-    end 
+  holiday_hash.each do |season, data|
+  puts "#{season.to_s.capitalize!}:"
+  data.each do |holiday, supply|
+    array = holiday.to_s.split("_")
+    final_holiday = []
+      array.each do |x|
+        final_holiday << x.capitalize!
+      end
+    holiday = final_holiday.join(" ")
+    supply = supply.join(", ")
+    puts "  #{holiday}: #{supply}"
+    end
   end
 end
 
